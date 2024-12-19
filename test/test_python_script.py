abf95ea5dca37bfdec6f3f540a8015cfaf73316f
Maik Fröbe <maik.froebe@uni-jena.de>
2024-12-19 08:18:37 +0100

    improve tests

--- test/test_python_script.py
+++ test/test_python_script.py
@@ -4,11 +4,21 @@ import pathlib
 import subprocess
 import tempfile
 import unittest
+import zipfile
+from pathlib import Path
 
 from approvaltests import verify_as_json
 
-ROOT_DIR = pathlib.Path(__file__).parent.parent.resolve()
-EXAMPLE_DIR = ROOT_DIR / "examples"
+TEST_RESOURCES = pathlib.Path(__file__).parent.parent.resolve() / "test" / "test-resources.zip"
+
+
+def resource(resource_name):
+    with tempfile.TemporaryDirectory(delete=False) as f:
+        with zipfile.ZipFile(TEST_RESOURCES, 'r') as zip_ref:
+            zip_ref.extractall(f)
+            ret = Path(f) / resource_name
+            assert ret.is_dir(), ret
+            return ret
 
 
 def run_command_and_return_persisted_metadata(command):
@@ -18,7 +28,7 @@ def run_command_and_return_persisted_metadata(command):
         actual = json.load(open(f"{f}/.ir-metadata", "r"))
         actual["sys"]["executable"] = "python3" if "python3" in actual["sys"]["executable"] else "UNEXPECTED"
         actual["sys"]["version_info"] = "3.XY.XY" if actual["sys"]["version_info"].startswith("3.") else "UNEXPECTED"
-        actual["sys"]["argv"] = [i.split("/examples/")[1] for i in actual["sys"]["argv"] if "/examples/" in i]
+        actual["sys"]["argv"] = [i.split("/")[-1] for i in actual["sys"]["argv"] if "example" in i]
         actual["sys"]["modules"] = [i for i in actual["sys"]["modules"] if "terrier" in i]
         actual["pkg_resources"] = [i for i in actual["pkg_resources"] if "python-terrier" in i]
         return actual
@@ -26,5 +36,7 @@ def run_command_and_return_persisted_metadata(command):
 
 class PythonScriptApprovalTests(unittest.TestCase):
     def test_for_valid_git_repo(self):
-        actual = run_command_and_return_persisted_metadata(lambda i: [f"{EXAMPLE_DIR}/pyterrier/example-script.py", i])
+        pyterrier_dir = resource('pyterrier')
+
+        actual = run_command_and_return_persisted_metadata(lambda i: ["python3", f"{pyterrier_dir}/example-script.py", i])
         verify_as_json(actual)
