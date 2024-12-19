d6a95545c11644d65bc0b52b93b8cc2c78dcdd0e
Maik Fröbe <maik.froebe@uni-jena.de>
2024-12-19 11:33:47 +0100

    add test for get_gpus

--- test/test_utility_functions.py
+++ test/test_utility_functions.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 from git import InvalidGitRepositoryError
 
-from autometadata import _executed_file_from_stacktrace, collect_git_repo_metadata
+from autometadata import _executed_file_from_stacktrace, collect_git_repo_metadata, get_gpu_info
 
 ROOT_DIR = pathlib.Path(__file__).parent.parent.resolve()
 
@@ -26,3 +26,6 @@ class TestUtilityFunctions(unittest.TestCase):
 
     def test_with_current_git_repo_multiple_nonroot_level(self):
         self.assertIsNotNone(collect_git_repo_metadata(ROOT_DIR / "src" / "autometadata"))
+
+    def test_get_gpus(self):
+        self.assertIsNotNone(get_gpu_info())
